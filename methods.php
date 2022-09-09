cb543ee8c061cf27f3aae221aa60a2452db536bb
caloy0714 <58654567+caloy0714@users.noreply.github.com>
2022-09-09 09:15:53 +0800

    Update methods.php

--- methods.php
+++ methods.php
@@ -1,12 +1,11 @@
 <?php
 
-class File
+class Registration
 {
-	protected $id;
 	protected $complete_name;
 	protected $email;
-	protected $picture;
-	protected $date;
+	protected $picture_path;
+	protected $registered_at;
 
 	const TYPE_DOCUMENT = 'document';
 	const TYPE_IMAGE = 'image';
@@ -15,47 +14,67 @@ class File
 	const DIRECTORY_IMAGES = 'images/';
 
 	public function __construct(
-		$id,
 		$complete_name,
 		$email,
-        $picture,
-        $date
+        $picture_path,
+        $registered_at =null
 	)
 	{
-		$this->id = $label;
 		$this->complete_name = $complete_name;
 		$this->email = $email;
-        $this->picture = $picture;
-        $this->date = $date;
+        $this->picture_path = $picture_path;
+        $this->created_at = $registered_at;
 	}
 
-	public function getLabel()
+    
+    public function index()
+    {
+        $register = [];
+        try {
+            $register = Registration::all();
+        } catch (Exception $e) {
+            $request->session()->flash('error', $e->getMessage());
+        }
+    }
+
+	public function getID()
 	{
-		return $this->label;
+		return $this->id;
 	}
 
-	public function getPath()
+	public function getCompleteName()
 	{
-		return $this->path;
+		return $this->complete_name;
 	}
 
-	public function getType()
+	public function getEmail()
 	{
-		return $this->type;
+		return $this->email;
 	}
 
+    public function getPicturePath()
+	{
+		return $this->picture_path;
+	}
+    
+    public function getRegisteredAt()
+	{
+		return $this->registered_at;
+	}
+    
 	public function save()
 	{
 		global $pdo;
 		try {
 
-			$sql = "INSERT INTO files SET label=:label, path=:path, type=:type";
+			$sql = "INSERT INTO files SET complete_name=:complete_name, email=:email, picture_path=:picture_path, registered_at=:registered_at";
 			$statement = $pdo->prepare($sql);
 
 			return $statement->execute([
-				':label' => $this->getLabel(),
-				':path' => $this->getPath(),
-				':type' => $this->getType()
+				':complete_name' => $this->getCompleteName(),
+				':email' => $this->getEmail(),
+                ':registered_at' => $this->getRegisteredAt(),
+                ':picture_path' => $this->getPicturePath()
 			]);
 
 		} catch (Exception $e) {
